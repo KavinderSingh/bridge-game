b64af72472fb157fa1127d4cb0f9e3c178e668ad
KavinderSingh <51209505+KavinderSingh@users.noreply.github.com>
2020-04-19 13:35:44 +0530

    Add files via upload

--- mini-takeshi-balance/movement.js
+++ mini-takeshi-balance/movement.js
@@ -1,36 +1,81 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // for(var i = 0; i < 10; i++) {
-    //     var div = document.getElementById(i);
-    //     // console.log(i);
-    //     setTimeout(change, 3);
-    // }
 
-    var i = 1;
-    setInterval(function change() {
-        if (i == 10){
-            alert("you win");
-            return;
-        }
-        var div = document.getElementById(i);
+
+//     var i = 1;
+//     setInterval(function change() {
+//         if (i == 10){
+//             alert("you win");
+//             return;
+//         }
+//         var div = document.getElementById(i);
        
-        // console.log(i);
-        // setTimeout(change, 3);
-        div.style.backgroundColor = "white";
-        document.getElementById(i-1).style.backgroundColor = "blue";
-        i = i + 1;
-        console.log(i);
-        
-}, 1000);
-
-// function change(i) {
-//         if (i == 10)break;
-//         var div = document.getElementById(i+=1);
 //         // console.log(i);
 //         // setTimeout(change, 3);
-//         div.style.color = "black";
+//         div.style.backgroundColor = "white";
+//         document.getElementById(i-1).style.backgroundColor = "blue";
+//         i = i + 1;
 //         console.log(i);
         
-// }
+// }, 1000);
+
+
+// let player = document.getElementById(5);
+// player.style.backgroundColor = "white";
+
+var i = -1;
+var div = document.getElementById(i);
+div.style.backgroundColor = "white";
+
+
+function movt(event) {
+    event.preventDefault();
+    if(event.keyCode === 38) {
+        i = i + 1;
+        var div = document.getElementById(i);
+
+        if (i == 10) {
+            div.style.backgroundColor = "white";
+            document.getElementById(i-1).style.backgroundColor = "blue";
+            alert('you win');
+            return;
+        }
+
+        if (i > 10) {
+            i = 10;
+        }
+
+        if (div != null) {
+            console.log(div);
+            div.style.backgroundColor = "white";
+            document.getElementById(i-1).style.backgroundColor = "blue";
+        }        
+        
+
+       
+    }
+    else if (event.keyCode === 40) {
+
+        i = i - 1;
+        var div = document.getElementById(i);
+        
+        if (i < -1) {
+            i = -1;
+        }
+        
+        if (div != null) {
+            console.log(div);
+            div.style.backgroundColor = "white";
+            document.getElementById(i+1).style.backgroundColor = "blue";
+        }  
+
+       
+    }
+    
+}
+
+document.addEventListener('keyup', movt);
+
+
 
 
 
